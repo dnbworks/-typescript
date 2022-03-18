9d903cae5384e8c386ee5e604c517f2b36cfbee8
dnbworks <dooyongnsaakobalderama@gmail.com>
2022-03-18 17:15:45 +0800

    finalize typescript

--- classes.ts
+++ classes.ts
@@ -1,8 +1,10 @@
 class Car {
     model: string
     color: string
-    age: number
-    peopleInPolicy: string[] = []
+    public age: number
+    private peopleInPolicy: string[] = []
+    readonly manufactorDate: string = "2010-12-5";
+    private readonly Distributor: string = "toyota";
     constructor(model, color, age){
         this.model = model;
         this.color = color;
@@ -16,8 +18,40 @@ class Car {
     addToPolicy(name: string){
         this.peopleInPolicy.push(name);
     }
+
+    getPolicy(){
+        console.log(this.peopleInPolicy);
+    }
 }
 
 const myJaguar = new Car("Jaguar", "green", 2);
 
-myJaguar.addToPolicy('John')
+myJaguar.addToPolicy('John');
+myJaguar.peopleInPolicy.push('Alan'); // can't push data coz its a private property
+myJaguar.getPolicy();
+
+// myJaguar.manufactorDate = "2012-12-5"; // cant change coz its a readonly property
+
+
+// clean up class
+
+// class Vehicle {
+
+//     private peopleInPolicy: string[] = []
+//     readonly manufactorDate: string = "2010-12-5";
+//     private readonly Distributor: string = "toyota";
+
+//     constructor(public model: string, public color: string, public age: number){}
+
+//     describe(){
+//         console.log(`A ${this.age} year old ${this.color} ${this.model}`);
+//     }
+
+//     addToPolicy(name: string){
+//         this.peopleInPolicy.push(name);
+//     }
+
+//     getPolicy(){
+//         console.log(this.peopleInPolicy);
+//     }
+// }
--- index.ts
+++ index.ts
@@ -1,161 +1,26 @@
 
-// NUMBER TYPE
+class Vehicle {
 
-let age = 32;
+    private peopleInPolicy: string[] = []
+    readonly manufactorDate: string = "2010-12-5";
+    private readonly Distributor: string = "toyota";
 
-let weight: number = 170;
+    constructor(public model: string, public color: string, public age: number){}
 
-weight = 180;
-weight = true;
+    describe(){
+        console.log(`A ${this.age} year old ${this.color} ${this.model}`);
+    }
 
-// STRING TYPE
+    addToPolicy(name: string){
+        this.peopleInPolicy.push(name);
+    }
 
-let myName = 'DNB';
-let city: string = 'London';
-
-myName = myName.length;
-
-// BOOLEAN TYPE
-
-let isAwesome = false;
-let isBoolean = 'London'.length === 0;
-let isBoring: boolean = false;
-
-
-// inference vs assignment
-// we usually go with inference
-
-// this is the case when assignment can be benefically
-
-let address: string;
-
-address = 'London';
-address = true;
-
-
-let amount;
-
-amount = 120;
-amount = true;
-amount = [];
-
-let passcode: string | number = 123;
-
-// OBJECT 
-
-const obj = {
-    firstname: 'john',
-    lasname: 'peter',
-    phone: '09090',
-    age: 17,
-    gender: true
+    getPolicy(){
+        console.log(this.peopleInPolicy);
+    }
 }
 
-const obj2: {
-    firstname: string;
-    lasname: string;
-    phone: string;
-    age: number;
-    gender: boolean;
-} = {
-    firstname: '',
-    lasname: '',
-    phone: '',
-    age: 17,
-    gender: true
-}
+const myToyota: Vehicle = new Vehicle("Toyota", "Red", 3);
 
-obj2.age = 34;
-obj2.age = true;
-
-obj2.name;
-
-obj2.phone.length;
-
-// ARRAYS
-
-const arrayOfString: (string | boolean)[] = ["string1", "string2", "string3"];
-
-arrayOfString.push("string4");
-arrayOfString.push(true);
-
-const arrayOfBooleans: boolean[] = [true, true, false];
-
-const arrayOfNumebers = [1, 2, 3];
-
-let arrayOfBOjects: {
-    firstname: string;
-    lasname: string;
-    phone: string;
-    age: number;
-    gender: boolean;
-}[] = [];
-
-arrayOfBOjects.push({
-    firstname: '',
-    lasname: '',
-    phone: '',
-    age: 34,
-    gender: true,
-});
-
-const arrayOfArrays = [["",""], ["",""]];
-
-// FUNCTIONS
-
-const add = (num1, num2) => {
-    return num1 + num2;
-}
-
-const subtract = (num1: number, num2: number) => {
-    return num1 - num2;
-}
-
-const compare = (num1: number, num2: number): boolean => {
-    return num1 > num2;
-}
-
-const log = (): void => {
-    console.log("hello")
-}
-
-// UNION TYPE
-
-let numberOrString: string | number = "James Bond";
-
-numberOrString = 4;
-
-let array: (string | number)[] = ["", 4, null, undefined];
-
-let object: {
-    hello: string | number
-}
-
-/////////////////////////////////////
-/// literal types 
-
-let rainbowColor: "red" | "orange" | "yellow" | "blue" | "indigo" | "violet" = "red";
-
-rainbowColor = "pink";
-
-rainbowColor = "yellow";
-
-enum RainBowTypes {
-    RED,
-    ORANGE,
-    YELLOW,
-    BLUE,
-    INDIGO,
-    VIOLOT
-}
-
-let rainbowColor2: RainBowTypes = RainBowTypes.BLUE;
-
-// OPTIONAL PROPERTY
-
-let obj4: {
-    property: string
-    property2?: string
-} = {
-    property: ''
-}
+myToyota.addToPolicy("James");
+myToyota.getPolicy();
